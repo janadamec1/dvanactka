c3cea397c0b0e7f08ddd55d550d885fe918aefbd
Jan Adamec on Mac <jadamec@gmail.com>
2016-10-30 17:58:01 +0100

    Initial Commit

---
+++ Dvanactka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dvanactka
+//
+//  Created by Jan Adamec on 30.10.16.
+//  Copyright © 2016 Jan Adamec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
