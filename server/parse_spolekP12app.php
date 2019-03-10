95a9b6bb36000583fc3939d3a3189de8dad22f94
Jan Adamec on Mac <jadamec@gmail.com>
2019-03-10 19:40:24 +0100

    Fix dvanactka calendar import

--- server/parse_spolekP12app.php
+++ server/parse_spolekP12app.php
@@ -11,7 +11,7 @@ foreach ($nodes as $i => $node) {
 	if ($nodeTitle != NULL) {
 		$title = $nodeTitle->nodeValue;
 		$aNewRecord = array("title" => $title);
-	
+
 		$nodeDate = firstItem($xpath->query("pubDate", $node));
 		if ($nodeDate != NULL) {
 			$date = date_create_from_format(DateTime::RSS, $nodeDate->nodeValue);
@@ -23,13 +23,13 @@ foreach ($nodes as $i => $node) {
 			$link = $nodeLink->nodeValue;
 			$aNewRecord["infoLink"] = $link;
 		}
-			
+
 		$nodeText = firstItem($xpath->query("description", $node));
 		if ($nodeText != NULL) {
 			$text = $nodeText->nodeValue;
 			$aNewRecord["text"] = $text;
 		}
-		
+
 		$nodeAuthor = firstItem($xpath->query("dc:creator", $node));
 		if ($nodeAuthor != NULL) {
 			$author = $nodeAuthor->nodeValue;
@@ -51,7 +51,7 @@ if (count($arrItems) > 0) {
 		$encoded .= json_encode($item, JSON_UNESCAPED_UNICODE);
 		$encoded .= ",\n";
 	}
-	
+
 	$filename = "items_spolekP12app.json";
 	file_put_contents($filename, $encoded, LOCK_EX);
 	chmod($filename, 0644);
@@ -65,53 +65,79 @@ $arrItems = array();
 $dom = new DomDocument;
 $dom->load("http://dvanactka.info/?plugin=all-in-one-event-calendar&controller=ai1ec_exporter_controller&action=export_events&xml=true");	// XML
 $xpath = new DomXPath($dom);
-$nodes = $xpath->query("//vevent");
+//$nodes = $xpath->query("//vevent/properties"); //somehow XPath does not work
+$nodes = $dom->getElementsByTagName('properties');
 foreach ($nodes as $i => $node) {
-	$nodeTitle = firstItem($xpath->query("summary", $node));
+	//$nodeTitle = firstItem($xpath->query("summary", $node));
+	$nodeTitle = firstItem($node->getElementsByTagName('summary'));
 	if ($nodeTitle != NULL) {
-		$title = $nodeTitle->nodeValue;
+		$title = $nodeTitle->textContent;
 		$aNewRecord = array("title" => $title);
-		
-		$nodeText = firstItem($xpath->query("description", $node));
+
+		//$nodeText = firstItem($xpath->query("description", $node));
+		$nodeText = firstItem($node->getElementsByTagName('description'));
 		if ($nodeText != NULL)
-			$aNewRecord["text"] = $nodeText->nodeValue;
+			$aNewRecord["text"] = $nodeText->textContent;
 
-		$nodeDate = firstItem($xpath->query("dtstart", $node));
+		//$nodeDate = firstItem($xpath->query("dtstart", $node));
+		$nodeDate = firstItem($node->getElementsByTagName('dtstart'));
 		if ($nodeDate != NULL) {
-			$sDateText = $nodeDate->nodeValue;
-			if (strpos($sDateText, "T") !== FALSE)
-				$date = date_create_from_format("Ymd\THis", $sDateText);
-			else
-				$date = date_create_from_format("!Ymd", $sDateText);
-			$aNewRecord["date"] = date_format($date, "Y-m-d\TH:i");
+  		$nodeDateDT = firstItem($nodeDate->getElementsByTagName('date-time'));
+      if ($nodeDateDT != NULL)
+        $sDateText = $nodeDateDT->nodeValue;
+      else
+        $sDateText = $nodeDate->textContent;
+      if (strpos($sDateText, "T") !== FALSE)
+        $date = date_create_from_format("Y-m-d\TH:i:s", $sDateText);
+      else
+        $date = date_create_from_format("!Y-m-d", $sDateText);
+      if ($date === FALSE)
+        echo "Error parsing dtstart " . $sDateText . "\n";
+      else
+        $aNewRecord["date"] = date_format($date, "Y-m-d\TH:i");
 		}
-		$nodeDateTo = firstItem($xpath->query("dtend", $node));
+		//$nodeDateTo = firstItem($xpath->query("dtend", $node));
+		$nodeDateTo = firstItem($node->getElementsByTagName('dtend'));
 		if ($nodeDateTo != NULL) {
-			$sDateText = $nodeDateTo->nodeValue;
-			if (strpos($sDateText, "T") !== FALSE)
-				$date = date_create_from_format("Ymd\THis", $sDateText);
-			else
-				$date = date_create_from_format("!Ymd", $sDateText);
-			$aNewRecord["dateTo"] = date_format($date, "Y-m-d\TH:i");
+  		$nodeDateToDT = firstItem($nodeDateTo->getElementsByTagName('date-time'));
+      if ($nodeDateToDT != NULL)
+        $sDateText = $nodeDateToDT->nodeValue;
+      else
+        $sDateText = $nodeDateTo->textContent;
+      if (strpos($sDateText, "T") !== FALSE)
+        $date = date_create_from_format("Y-m-d\TH:i:s", $sDateText);
+      else
+        $date = date_create_from_format("!Y-m-d", $sDateText);
+      if ($date === FALSE)
+        echo "Error parsing dtend " . $sDateText . "\n";
+      else
+        $aNewRecord["dateTo"] = date_format($date, "Y-m-d\TH:i");
 		}
-		
-		$nodeAddress = firstItem($xpath->query("location", $node));
-		if ($nodeAddress != NULL)
-			$aNewRecord["address"] = $nodeAddress->nodeValue;
 
-		$nodeGeo = firstItem($xpath->query("geo", $node));
+		//$nodeAddress = firstItem($xpath->query("location", $node));
+		$nodeAddress = firstItem($node->getElementsByTagName('location'));
+		if ($nodeAddress != NULL)
+			$aNewRecord["address"] = $nodeAddress->textContent;
+
+		//$nodeGeo = firstItem($xpath->query("geo", $node));
+		$nodeGeo = firstItem($node->getElementsByTagName('geo'));
 		if ($nodeGeo != NULL) {
-			$arrGeo = explode(";", $nodeGeo->nodeValue);
-			if (count($arrGeo) > 1) {
-				$aNewRecord["locationLat"] = $arrGeo[0];
-				$aNewRecord["locationLong"] = $arrGeo[1];
+		  //$nodeLat = firstItem($xpath->query("latitude", $nodeGeo));
+		  //$nodeLong = firstItem($xpath->query("longitude", $nodeGeo));
+  		$nodeLat = firstItem($nodeGeo->getElementsByTagName('latitude'));
+  		$nodeLong = firstItem($nodeGeo->getElementsByTagName('longitude'));
+			if ($nodeLat != NULL && $nodeLong != NULL) {
+				$aNewRecord["locationLat"] = $nodeLat->nodeValue;
+				$aNewRecord["locationLong"] = $nodeLong->nodeValue;
 			}
 		}
-		
+
 		$sFilter = "Dvanáctka.info";
-		$nodeContact = firstItem($xpath->query("contact", $node));
+		//$nodeContact = firstItem($xpath->query("contact", $node));
+		$nodeContact = firstItem($node->getElementsByTagName('contact'));
 		if ($nodeContact != NULL) {
-			$arrContact = explode(";", $nodeContact->nodeValue);
+		  //echo $nodeContact->textContent. "\n";
+			$arrContact = explode(";", $nodeContact->textContent);
 			foreach ($arrContact as $j => $item) {
 				$contact = trim($item);
 				if (strlen($contact) == 0) continue;
@@ -138,7 +164,7 @@ if (count($arrItems) > 0) {
 		$encoded .= json_encode($item, JSON_UNESCAPED_UNICODE);
 		$encoded .= ",\n";
 	}
-	
+
 	$filename = "items_P12app_calendar.json";
 	file_put_contents($filename, $encoded, LOCK_EX);
 	chmod($filename, 0644);
