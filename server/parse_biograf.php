25e80bb7cf69570c7ec335928c8984e545482d68
Jan Adamec on Mac <jadamec@gmail.com>
2019-09-07 21:18:20 +0200

    Fix biograf parsing (new cinema operator)

--- server/parse_biograf.php
+++ server/parse_biograf.php
@@ -1,40 +1,31 @@
 <?php
-
-function firstItem($arrNodes) {
-	if ($arrNodes === NULL || $arrNodes === FALSE) return NULL;
-	return $arrNodes->item(0);
-}
-
-/* Set HTTP response header to plain text for debugging output */
-header("Content-type: text/plain");
-/* Use internal libxml errors -- turn on in production, off for debugging */
-libxml_use_internal_errors(true);
+include_once "parse_common.php";
 
 $sAddress = "Modřanský biograf\nU Kina 1\n143 00 Praha 12 - Modřany";
 
 $arrItems = array();
 $dom = new DomDocument;
-$dom->loadHTMLFile("http://www.modranskybiograf.cz/klient-349/kino-114/");
+$dom->loadHTMLFile("http://www.modranskybiograf.cz/klient-2774/kino-433/stranka-14487");
 $xpath = new DomXPath($dom);
-$nodes = $xpath->query("//div[@class='calendar-left-table-tr']/a[@class='cal-event-item shortName']");
+$nodes = $xpath->query("//div[@class='calendar-left-table-tr']");
 foreach ($nodes as $i => $node) {
-	$nodeTitle = firstItem($xpath->query("h2", $node));
+	$nodeTitle = firstItem($xpath->query("h2/a[@class='cal-event-item shortName']", $node));
 	if ($nodeTitle != NULL) {
 		$title = $nodeTitle->nodeValue;
 		if ($title == "KINO NEHRAJE")
 			continue;
-		
-		$link = $node->getAttribute("href");
+
+		$link = $nodeTitle->getAttribute("href");
 		if (substr($link, 0, 4) != "http") {
 			$link = "http://www.modranskybiograf.cz" . $link;
 		}
 		$aNewRecord = array("title" => $title);
 		$aNewRecord["infoLink"] = $link;
-		
-		$aNewRecord["text"] = implode(" | ", explode("\n", $node->getAttribute("title")));
+
+		//$aNewRecord["text"] = implode(" | ", explode("\n", $node->getAttribute("title")));
 		$aNewRecord["address"] = $sAddress;
-		
-		$nodeBuyLink = firstItem($xpath->query("..//a[@class='cal-event-item-buy-span']", $node));
+
+		$nodeBuyLink = firstItem($xpath->query("div[@class='ap_price']//a[@class='cal-event-item-buy-span icon-shopping-cart']", $node));
 		if ($nodeBuyLink != NULL) {
 			$aNewRecord["buyLink"] = $nodeBuyLink->getAttribute("href");
 		}
@@ -49,12 +40,12 @@ foreach ($nodes as $i => $node) {
 					$date = date_add($date, new DateInterval('P1Y'));
 				}
 				$aNewRecord["date"] = date_format($date, "Y-m-d\TH:i");
-		    }
+		  }
 		}
-		
+
 		if (array_key_exists("date", $aNewRecord))
 	    	array_push($arrItems, $aNewRecord);
-    }
+  }
 }
 if (count($arrItems) > 0) {
 	$arr = array("items" => $arrItems);
@@ -64,5 +55,5 @@ if (count($arrItems) > 0) {
 	chmod($filename, 0644);
 }
 //echo $encoded;
-echo "done.";
+echo "Biograf done, " . count($arrItems) . " items\n";
 ?>
